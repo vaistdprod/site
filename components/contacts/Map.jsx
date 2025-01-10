bc981204cbdec80e94caa061aad256c2787caf7b
Matěj Vais <vais@tdprod.cz>
2025-01-10 21:59:46 +0100

    Launch v1.2

--- components/contacts/Map.jsx
+++ components/contacts/Map.jsx
@@ -1,46 +1,50 @@
 'use client';
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import Script from 'next/script';
 
 export default function Map() {
   const mapRef = useRef(null);
 
-  function initMap() {
-    if (window.google && mapRef.current) {
-      const map = new window.google.maps.Map(mapRef.current, {
-        center: { lat: 49.83393862369578, lng: 18.29108796768538 },
-        zoom: 16,
-      });
+  useEffect(() => {
+    window.initMap = () => {
+      if (window.google && mapRef.current) {
+        const { Map } = google.maps;
+        const { AdvancedMarkerElement } = google.maps.marker;
 
-      const marker = new window.google.maps.Marker({
-        position: { lat: 49.83393862369578, lng: 18.29108796768538 },
-        map,
-        title: 'TD Productions',
-      });
+        const map = new Map(mapRef.current, {
+          center: { lat: 49.83393862369578, lng: 18.29108796768538 },
+          zoom: 16,
+        });
 
-      const contentString = `
-        <div style="font-family: Plus Jakarta Sans;">
-          <h6 class="mb-5 text-black">TD Productions</h6>
-          <p class="text-black">28. října 205/45, 702 00 Ostrava</p>
-        </div>
-      `;
+        const marker = new AdvancedMarkerElement({
+          position: { lat: 49.83393862369578, lng: 18.29108796768538 },
+          map,
+          title: 'TD Productions',
+        });
 
-      const infoWindow = new google.maps.InfoWindow({
-        content: contentString,
-      });
+        const contentString = `
+          <div style="font-family: Plus Jakarta Sans;">
+            <h6 class="mb-5 text-black">TD Productions</h6>
+            <p class="text-black">28. října 205/45, 702 00 Ostrava</p>
+          </div>
+        `;
+        const infoWindow = new google.maps.InfoWindow({ content: contentString });
 
-      infoWindow.open(map, marker);
-    }
-  }
+        infoWindow.open({ anchor: marker, map });
+      }
+    };
+  }, []);
 
   return (
     <>
       <Script
-        src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}`}
+        src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&callback=initMap&libraries=marker`}
         strategy="afterInteractive"
-        onLoad={initMap}
       />
-      <div style={{ width: '100%', height: '600px' }} ref={mapRef} />
+      <div
+        ref={mapRef}
+        style={{ width: '100%', height: '600px' }}
+      />
     </>
   );
 }
