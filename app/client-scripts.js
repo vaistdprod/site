e405c0ebf6ae588537838aea777aa9925d8121d8
Matěj Vais <vais@tdprod.cz>
2025-01-11 08:31:59 +0100

    Launch v1.2.7

---
+++ app/client-scripts.js
@@ -0,0 +1,39 @@
+"use client";
+import { useEffect, useState } from "react";
+import Script from "next/script";
+
+export default function ClientScripts() {
+  const [isNonTouch, setIsNonTouch] = useState(false);
+
+  useEffect(() => {
+    const hasTouch =
+      typeof window !== "undefined" &&
+      ("ontouchstart" in window || navigator.maxTouchPoints > 0);
+    setIsNonTouch(!hasTouch);
+  }, []);
+
+  return (
+    <>
+      <Script
+        src="https://kit.fontawesome.com/e630efef3e.js"
+        crossOrigin="anonymous"
+        strategy="afterInteractive"
+      />
+      <Script src="/assets/js/plugins.js" strategy="afterInteractive" />
+      <Script src="/assets/js/gsap.min.js" strategy="afterInteractive" />
+      <Script
+        src="//js-eu1.hs-scripts.com/145304620.js"
+        id="hubspot-script-loader"
+        strategy="afterInteractive"
+      />
+      {isNonTouch && (
+        <>
+          <Script src="/assets/js/ScrollTrigger.min.js" strategy="afterInteractive" />
+          <Script src="/assets/js/ScrollSmoother.min.js" strategy="afterInteractive" />
+          <Script src="/assets/js/smoother-script.js" strategy="lazyOnload" />
+          <Script src="/assets/js/scripts.js" strategy="afterInteractive" />
+        </>
+      )}
+    </>
+  );
+}
--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,13 @@
-import './globals.css';
-import Script from 'next/script';
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import ClientScripts from "./client-scripts";
 
 export const metadata = {
-  title: 'TD Productions | Posouváme hranice digitální inovace',
-  description: 'Inovace se u nás snoubí s profesionalitou a jednohlasným zájmem posunout váš projekt na tu nejvyšší úroveň. Kontaktujte nás ještě dnes a získejte nabídku.',
+  title: "TD Productions | Posouváme hranice digitální inovace",
+  description: "Inovace se u nás snoubí s profesionalitou a jednohlasným zájmem posunout váš projekt na tu nejvyšší úroveň. Kontaktujte nás ještě dnes a získejte nabídku.",
   icons: {
-    icon: '/assets/imgs/favicon.ico',
-    shortcut: '/assets/imgs/favicon.ico',
+    icon: "/assets/imgs/favicon.ico",
+    shortcut: "/assets/imgs/favicon.ico",
   },
 };
 
@@ -15,9 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="cs">
       <head>
-        <Script
-          id="google-tag-manager"
-          strategy="beforeInteractive"
+        <script
           dangerouslySetInnerHTML={{
             __html: `
               (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
@@ -35,27 +33,11 @@ export default function RootLayout({ children }) {
             src="https://www.googletagmanager.com/ns.html?id=GTM-P73P3DPX"
             height="0"
             width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          ></iframe>
+            style={{ display: "none", visibility: "hidden" }}
+          />
         </noscript>
         {children}
-        <Script
-          src="https://kit.fontawesome.com/e630efef3e.js"
-          crossOrigin="anonymous"
-          strategy="afterInteractive"
-        />
-        <Script src="/assets/js/plugins.js" strategy="afterInteractive" />
-        <Script src="/assets/js/gsap.min.js" strategy="afterInteractive" />
-        <Script src="/assets/js/ScrollTrigger.min.js" strategy="afterInteractive" />
-        <Script src="/assets/js/ScrollSmoother.min.js" strategy="afterInteractive" />
-        <Script src="/assets/js/isotope.pkgd.min.js" strategy="afterInteractive" />
-        <Script src="/assets/js/smoother-script.js" strategy="lazyOnload" />
-        <Script src="/assets/js/scripts.js" strategy="afterInteractive" />
-        <Script
-          src="//js-eu1.hs-scripts.com/145304620.js"
-          id="hubspot-script-loader"
-          strategy="afterInteractive"
-        />
+        <ClientScripts />
         <SpeedInsights />
       </body>
     </html>
