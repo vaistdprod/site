e32baf1904912f604c5adda98b81508fff9aa81a
Matěj Vais <vais@tdprod.cz>
2025-02-16 19:43:32 +0100

    Implement Typescript v2.0.3

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -38,51 +38,59 @@ type Metadata = {
   };
 };
 
-export async function generateMetadata({ params }: { params: { slug: string } }): Promise<Metadata> {
+export async function generateMetadata({ params }: { params: { slug: string } }) {
+    const { slug } = params;
+    const post = await getPostBySlug(slug);
+
+    if (!post) {
+      notFound();
+    }
+
+     const dynamicKeywords =
+    post.keywords && post.keywords.length > 0
+      ? post.keywords
+      : ['TD Productions', 'Blog'];
+
+
+    return {
+        title: post.title + " | TD Productions",
+        description: post.excerpt,
+        keywords: dynamicKeywords,
+        openGraph: {
+            title: post.title,
+            description: post.excerpt,
+            url: `https://tdprod.cz/blog/${slug}`,
+            images: [
+                {
+                    url: post.coverImage,
+                    width: 1200,
+                    height: 630,
+                    alt: post.title,
+                },
+            ],
+        },
+        twitter: {
+            card: 'summary_large_image',
+            title: post.title,
+            description: post.excerpt,
+            images: [post.coverImage],
+        },
+        alternates: {
+            canonical: `https://tdprod.cz/blog/${slug}`,
+        },
+    };
+}
+
+export default async function BlogPostPage({ params }: PageProps) {
   const { slug } = params;
   const post = await getPostBySlug(slug);
 
   if (!post) notFound();
 
-  if (!post) notFound();
+  const allPosts = await getAllPosts();
+  const latestPosts = allPosts.slice(0, 3);
 
-  return {
-    title: post.title,
-    description: post.content.substring(0, 160), // Limit description to 160 characters
-    keywords: post.tags,
-    openGraph: {
-      title: post.title,
-      description: post.content.substring(0, 160),
-      url: `/blog/${slug}`,
-      images: [
-        {
-          url: post.coverImage,
-          width: 1200,
-          height: 630,
-          alt: post.title,
-        },
-      ],
-    },
-    twitter: {
-      card: 'summary_large_image',
-      title: post.title,
-      description: post.content.substring(0, 160),
-      images: [post.coverImage],
-    },
-    alternates: {
-      canonical: `/blog/${slug}`,
-    },
-  };
-}
-
-export default async function BlogPostPage({ params }: PageProps) {
-    const { slug } = params;
-    const post = await getPostBySlug(slug);
-
-    if (!post) notFound();
-
-    const allPosts = await getAllPosts();
-    const latestPosts = allPosts.slice(0, 3);
-
-    return <ClientBlogPostPage post={post} latestPosts={latestPosts} />;
+  return (
+    <ClientBlogPostPage post={post} latestPosts={latestPosts} />
+  );
 }
