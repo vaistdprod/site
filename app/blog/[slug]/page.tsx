e7538088dafeae260b5eb7b72ed9d179d5bf9d44
Matěj Vais <vais@tdprod.cz>
2025-02-16 19:48:12 +0100

    Implement Typescript v2.0.5

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -82,7 +82,7 @@ export async function generateMetadata({ params }: { params: { slug: string } })
 }
 
 export default async function BlogPostPage({ params }: PageProps) {
-  const { slug } = params;
+  const { slug } = await params;
   const post = await getPostBySlug(slug);
 
   if (!post) notFound();
